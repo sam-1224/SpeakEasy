bea947d8889266e3a7509d1435d97661e1e1cdf7
Saumitra Dubey <saumitradybey1224@gmail.com>
2025-08-28 19:46:21 +0530

    Was Using = instead of === which was casuing uncaught window exception error resulting in blank ui TT...Finally fixed :D

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = (import.meta.env.MODE = "development"
-  ? "http://localhost:5001/api"
-  : "/api");
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 // This file is used to create an axios instance that can be used throughout the application
 // to make API calls. It sets the base URL for the API requests to the backend server.
 
