6a744d2335fbdcf3cfb78172f78cdc9663bc3b55
Saumitra Dubey <saumitradybey1224@gmail.com>
2025-09-02 14:42:18 +0530

    feat: Add friends page and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import PageLoader from './components/PageLoader.jsx'
 import useAuthUser from './hooks/useAuthUser.js'
 import Layout from './components/Layout.jsx'
 import { useThemeStore } from './store/useThemeStore.js'
+import FriendsPage from './Pages/FriendsPage.jsx'
 
 const App = () => {
   //  tank stack query
@@ -82,6 +83,19 @@ const App = () => {
           <Navigate to="/login" />
         )} />
       </Routes>
+
+      <Route
+        path="/friends"
+        element={
+          isAuthenticated && isOnboarded ? (
+            <Layout showSidebar={true}>
+              <FriendsPage />
+            </Layout>
+          ) : (
+            <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+          )
+        }
+      />
 
       <Toaster />
     </div>
